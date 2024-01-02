e040f4aa8f059a15c5178020dbeb2ec7df2f1a7e
David Rohweder <david01rohweder@gmail.com>
2024-01-01 23:37:35 -0700

    Initial Commit

---
+++ Alma/AlmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlmaApp.swift
+//  Alma
+//
+//  Created by David Rohweder on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct AlmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
